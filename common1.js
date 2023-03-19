06fd1828103282c63dfadc00751a7ef768262ecd
K-Ang-004 <128273771+K-Ang-004@users.noreply.github.com>
2023-03-19 12:22:24 +0800

    Add files via upload

---
+++ common1.js
@@ -0,0 +1,8 @@
+$(window).on('load', function() {
+    setTimeout(function(){
+        $('.loading').fadeOut();
+        $(window).scrollTop(0);
+    }, 2000)
+    // $('.loading').fadeOut(); 淡出
+    
+});
