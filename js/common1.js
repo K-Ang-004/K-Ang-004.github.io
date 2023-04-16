0d31d5af73bad34c177199db1d68fe2898819053
K-Ang-004 <128273771+K-Ang-004@users.noreply.github.com>
2023-04-16 21:45:04 +0800

    Add files via upload

---
+++ js/common1.js
@@ -0,0 +1,257 @@
+let mapx; // 定義一個新變數給地圖用
+
+function initMap() {
+  mapx = new google.maps.Map(document.getElementById("map"), {
+    // 把 id="map" 的div變成地圖
+    center: { lat: 25.0400836, lng: 121.512454 }, // 地圖中心點的經緯度
+    zoom: 9, // 縮放程度
+    styles:
+      [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.neighborhood",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#263c3f"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#6b9a76"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#38414e"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#212a37"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9ca5b3"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#1f2835"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#f3d19c"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#2f3948"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#515c6d"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        }
+      ]
+  });
+  var marker1 = new google.maps.Marker({ // 定義一個新圖釘
+    position: { lat: 25.0581228, lng: 121.5669957 }, // 圖釘的位置 
+    map: mapx, // 圖釘給哪個地圖用
+  });
+  var marker2 = new google.maps.Marker({
+    position: { lat: 25.0616608, lng: 121.4655508 },
+    map: mapx
+  });
+  var marker3 = new google.maps.Marker({
+    position: { lat: 24.944338, lng: 121.2068091 },
+    map: mapx
+  });
+  var marker4 = new google.maps.Marker({
+    position: { lat: 24.7973426, lng: 120.9602034 },
+    map: mapx
+  });
+}
+
+window.initMap = initMap;
+
+$("#W").click(function () {
+  $("#Wimg").show();
+  $("#Yimg").hide();
+  $("#Rimg").hide();
+});
+
+$("#Y").click(function () {
+  $("#Wimg").hide();
+  $("#Yimg").show();
+  $("#Rimg").hide();
+});
+
+$("#R").click(function () {
+  $("#Wimg").hide();
+  $("#Yimg").hide();
+  $("#Rimg").show();
+});
