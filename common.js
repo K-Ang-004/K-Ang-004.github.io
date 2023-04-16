43c696270ab7695dfe39d74a4005eced09ef996d
K-Ang-004 <128273771+K-Ang-004@users.noreply.github.com>
2023-04-16 16:57:16 +0800

    Delete common.js

--- common.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function() {
-    setTimeout(function(){
-        $('.loading').fadeOut();
-        $(window).scrollTop(0);
-    }, 2000)
-    // $('.loading').fadeOut(); 淡出
-    
-});
